f71dcdd6474c7cbe4b61fcd69a4ef5adad07f200
Mouise Bashir <74969971+mouise1111@users.noreply.github.com>
2023-07-08 21:21:48 +0200

    added facebook account icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -252,22 +252,22 @@
 <main class="lg:px-24 sm:w-full">
 	<div class=" lg:h-screen first-screen md:mb-16 lg:mb-0">
 		<Navbar />
-		<div class="lg:pt-16 pt-5">
-			<!-- <h1 class="text-6xl tracking-wider font-extrabold my-3 sm:text-sm">
+		<div class="pt-5 lg:pt-16">
+			<!-- <h1 class="my-3 text-6xl font-extrabold tracking-wider sm:text-sm">
 				Met een hart voor <span class="text-lime-600">Zorg</span>
 			</h1> -->
 			<h1
-				class="text-5xl md:text-5xl lg:text-6xl tracking-wider font-extrabold my-3 mx-2 lg:mx-0 title-1"
+				class="mx-2 my-3 text-5xl font-extrabold tracking-wider md:text-5xl lg:text-6xl lg:mx-0 title-1"
 			>
 				Met een hart voor <span class="text-lime-600">Zorg</span>
 			</h1>
 
 			<h2
-				class="text-4xl font-bold tracking-wider my-5 mx-2 lg:mx-0 lg:text-left text-right title-2"
+				class="mx-2 my-5 text-4xl font-bold tracking-wider text-right lg:mx-0 lg:text-left title-2"
 			>
 				Thuisverpleging
 			</h2>
-			<p class="lg:text-lg text-md lg:w-1/2 my-3 mx-2 lg:mx-0 paragraph-1">
+			<p class="mx-2 my-3 lg:text-lg text-md lg:w-1/2 lg:mx-0 paragraph-1">
 				Welkom bij Nova - Uw vertrouwde partner voor thuisverpleging. Onze toegewijde en ervaren
 				verpleegkundigen bieden gepersonaliseerde zorg en ondersteuning in de comfortabele omgeving
 				van uw eigen huis.<br /> Wij bedienen met trots de gemeente
@@ -277,7 +277,7 @@
 				<span class="font-semibold">Oudenaken</span>.
 			</p>
 			<button
-				class="py-3 px-5 bg-lime-600 hover:bg-lime-500 text-white rounded-lg my-3 contact-btn mx-2 lg:mx-0"
+				class="px-5 py-3 mx-2 my-3 text-white rounded-lg bg-lime-600 hover:bg-lime-500 contact-btn lg:mx-0"
 			>
 				<a href="#contact" on:click|preventDefault={scrollIntoView}>Contacteer ons</a>
 			</button>
@@ -285,108 +285,108 @@
 		<img
 			src="/female_doctor_2.png"
 			alt=""
-			class="absolute bottom-0 right-0 lg:w-1/2 z-10 mt-12 doc hidden lg:block"
+			class="absolute bottom-0 right-0 z-10 hidden mt-12 lg:w-1/2 doc lg:block"
 		/>
 		<img
 			src="/ellipse1.png"
 			alt="ellipse"
-			class="absolute bottom-0 right-0 lg:w-1/2 ellilpse hidden lg:block"
+			class="absolute bottom-0 right-0 hidden lg:w-1/2 ellilpse lg:block"
 		/>
 	</div>
 	<Services />
 	<div
 		id="team"
-		class="lg:h-screen flex items-center text-center lg:text-left third-screen lg:pt-0 mb-0 md:mb-16 lg:mb-0"
+		class="flex items-center mb-0 text-center lg:h-screen lg:text-left third-screen lg:pt-0 md:mb-16 lg:mb-0"
 	>
-		<div class="lg:flex flex-row rounded items-center justify-between lg:content-center">
-			<div class="lg:w-1/2 flex justify-center order-last lg:order-first">
+		<div class="flex-row items-center justify-between rounded lg:flex lg:content-center">
+			<div class="flex justify-center order-last lg:w-1/2 lg:order-first">
 				<img
 					src="https://online.norwich.edu/sites/default/files/content/resources/header/157.jpg"
 					alt="team"
-					class="rounded-3xl w-2/3 team-picture hidden lg:block"
+					class="hidden w-2/3 rounded-3xl team-picture lg:block"
 				/>
 			</div>
-			<div class="lg:w-1/2 lg:p-5 p-2 team-container">
+			<div class="p-2 lg:w-1/2 lg:p-5 team-container">
 				<h3 class=" bold text-lime-600">Tot uw dienst</h3>
-				<h2 class=" text-2xl my-4">Maak kennis met ons zorgzame team</h2>
-				<!-- <p class="text-md lg:w-1/2 my-3 mx-2 lg:mx-0"></p> -->
+				<h2 class="my-4 text-2xl">Maak kennis met ons zorgzame team</h2>
+				<!-- <p class="mx-2 my-3 text-md lg:w-1/2 lg:mx-0"></p> -->
 				<p class="text-md">
 					Ons toegewijde team staat volledig tot uw dienst om hoogwaardige zorg te bieden. We
 					begrijpen dat uw welzijn van het grootste belang is en streven ernaar om u de beste
 					zorgervaring te bieden. Met een team van ervaren professionals zijn we uitgerust om aan al
 					uw zorgbehoeften te voldoen.
 				</p>
-				<div class="mt-5 flex flew-row justify-center lg:justify-start">
+				<div class="flex justify-center mt-5 flew-row lg:justify-start">
 					<img src="/check-icon.png" alt="" />
-					<p class="bold text-md font-medium px-3">Experten</p>
+					<p class="px-3 font-medium bold text-md">Experten</p>
 				</div>
-				<div class="mt-3 flex flew-row justify-center lg:justify-start">
+				<div class="flex justify-center mt-3 flew-row lg:justify-start">
 					<img src="/check-icon.png" alt="" />
-					<p class="bold text-md font-medium px-3">+20 jaar ervaring</p>
+					<p class="px-3 font-medium bold text-md">+20 jaar ervaring</p>
 				</div>
 			</div>
 
-			<div class="w-5/6 flex justify-center order-last lg:hidden mx-auto mt-4">
+			<div class="flex justify-center order-last w-5/6 mx-auto mt-4 lg:hidden">
 				<img
 					src="https://online.norwich.edu/sites/default/files/content/resources/header/157.jpg"
 					alt="team"
-					class="rounded-3xl p-3 team-picture md:w-3/5 lg:hidden block"
+					class="block p-3 rounded-3xl team-picture md:w-3/5 lg:hidden"
 				/>
 			</div>
 		</div>
 	</div>
 	<div
 		id="review"
-		class=" mb-0 md:mb-32 sm fourth-screen lg:mb-0 lg:h-screen mt-0 lg:flex lg:flex-col lg:content-center lg:justify-center"
+		class="mt-0 mb-0 md:mb-32 sm fourth-screen lg:mb-0 lg:h-screen lg:flex lg:flex-col lg:content-center lg:justify-center"
 	>
-		<h3 class="text-center bold text-lime-600 mt-16 lg:mt-0 title-review">Onze Beoordeling</h3>
-		<!-- <h2 class="text-center text-2xl my-4 px-2">
+		<h3 class="mt-16 text-center bold text-lime-600 lg:mt-0 title-review">Onze Beoordeling</h3>
+		<!-- <h2 class="px-2 my-4 text-2xl text-center">
 			Nova heeft een ruim zorgaanbod voor elke persoon die aangesloten is bij een Belgische
 			mutualiteit.
 		</h2> -->
 		<div
-			class="lg:flex lg:flex-row justify-center lg:gap-8 lg:max-w-6xl lg:my-24 diensten mx-auto lg:justify-between container-review"
+			class="justify-center mx-auto lg:flex lg:flex-row lg:gap-8 lg:max-w-6xl lg:my-24 diensten lg:justify-between container-review"
 		>
 			<div
-				class="border-t-4 border-lime-600 rounded-md mx-auto shadow-lg w-80 lg:w-80 md:w-64 h-56 lg:h-64 py-5 lg:py-10 lg:my-0 my-8 text-center lg:text-left md:text-left flex flex-col gap-4 justify-center"
+				class="flex flex-col justify-center h-56 gap-4 py-5 mx-auto my-8 text-center border-t-4 rounded-md shadow-lg border-lime-600 w-80 lg:w-80 md:w-64 lg:h-64 lg:py-10 lg:my-0 lg:text-left md:text-left"
 			>
-				<h3 class="py-2 text-4xl lg:text-5xl text-center">6</h3>
+				<h3 class="py-2 text-4xl text-center lg:text-5xl">6</h3>
 				<h4 class="text-center">Geverifieerde specialisten</h4>
 			</div>
 			<div
-				class="border-t-4 border-lime-600 rounded-md mx-auto shadow-lg w-80 lg:w-80 md:w-64 h-56 lg:h-64 py-5 lg:py-10 lg:my-0 my-8 text-center lg:text-left md:text-left flex flex-col gap-4 justify-center"
+				class="flex flex-col justify-center h-56 gap-4 py-5 mx-auto my-8 text-center border-t-4 rounded-md shadow-lg border-lime-600 w-80 lg:w-80 md:w-64 lg:h-64 lg:py-10 lg:my-0 lg:text-left md:text-left"
 			>
-				<!-- <h3 class="py-2 text-4xl lg:text-5xl text-center" this:value={count} /> -->
-				<h3 class="py-2 text-4xl lg:text-5xl text-center">+50</h3>
+				<!-- <h3 class="py-2 text-4xl text-center lg:text-5xl" this:value={count} /> -->
+				<h3 class="py-2 text-4xl text-center lg:text-5xl">+50</h3>
 				<h4 class="text-center">Tevreden klanten</h4>
 			</div>
 			<div
-				class="border-t-4 border-lime-600 rounded-md mx-auto shadow-lg w-80 lg:w-80 h-56 md:w-64 lg:h-64 py-5 lg:py-10 lg:my-0 my-8 text-center lg:text-left md:text-left flex flex-col gap-4 justify-center"
+				class="flex flex-col justify-center h-56 gap-4 py-5 mx-auto my-8 text-center border-t-4 rounded-md shadow-lg border-lime-600 w-80 lg:w-80 md:w-64 lg:h-64 lg:py-10 lg:my-0 lg:text-left md:text-left"
 			>
-				<h3 class="py-2 text-4xl lg:text-5xl text-center">99,7%</h3>
+				<h3 class="py-2 text-4xl text-center lg:text-5xl">99,7%</h3>
 				<h4 class="text-center">Positieve feedback</h4>
 			</div>
 		</div>
 	</div>
 	<!-- <div
 		id="contact"
-		class="mb-0 md:mb-32 lg:mb-0 h-screen last-screen lg:flex lg:flex-col justify-center"
+		class="justify-center h-screen mb-0 md:mb-32 lg:mb-0 last-screen lg:flex lg:flex-col"
 	>
-		<h3 class="text-center font-bold text-lime-600 mt-16 lg:mt-0 contact-title1">Contacteer ons</h3>
-		<h2 class="text-center text-2xl my-4 mb-8 lg:mb-16 px-2 contact-title2">
+		<h3 class="mt-16 font-bold text-center text-lime-600 lg:mt-0 contact-title1">Contacteer ons</h3>
+		<h2 class="px-2 my-4 mb-8 text-2xl text-center lg:mb-16 contact-title2">
 			Wenst u thuisverpleging aan te vragen, gebruik te maken van onze ondersteunende diensten of
 			heeft u een vraag? Aarzel dan niet om ons te contacteren!
 		</h2>
 		<div class="flex flex-col lg:flex-row lg:justify-between sm:justify-center">
 			<div
-				class="flex flex-col lg:items-start items-center mb-8 lg:mb-0 lg:justify-center lg:min-w-fit lg:px-4 lg:ml-8 contact-container"
+				class="flex flex-col items-center mb-8 lg:items-start lg:mb-0 lg:justify-center lg:min-w-fit lg:px-4 lg:ml-8 contact-container"
 			>
-				<div class="flex flex-row items-center mb-4 justify-between">
+				<div class="flex flex-row items-center justify-between mb-4">
 					<img src="/phone-solid.svg" alt="" class="w-8 mx-2 lg:mx-0 lg:w-12 lg:mr-4" />
 					<a href="tel:+31612345678">+0473 95 28 64</a>
 				</div>
 
-				<div class="flex flex-row items-center mb-4 justify-between">
+				<div class="flex flex-row items-center justify-between mb-4">
 					<img src="/envelope-solid.svg" alt="" class="w-8 mx-2 lg:mx-0 lg:w-12 lg:mr-4" />
 
 					<a href="mailto:lyhxr@example.com">info@norwich.edu</a>
@@ -396,10 +396,10 @@
 				<form
 					action="https://formspree.io/f/xgejwpke"
 					method="POST"
-					class="max-w-lg lg:max-w-4xl mx-auto"
+					class="max-w-lg mx-auto lg:max-w-4xl"
 				>
 					<div class="mb-4">
-						<label for="name" class="block text-gray-700 font-semibold">Naam</label>
+						<label for="name" class="block font-semibold text-gray-700">Naam</label>
 						<input
 							type="text"
 							id="name"
@@ -409,7 +409,7 @@
 						/>
 					</div>
 					<div class="mb-4">
-						<label for="email" class="block text-gray-700 font-semibold">E-mailadres</label>
+						<label for="email" class="block font-semibold text-gray-700">E-mailadres</label>
 						<input
 							type="email"
 							id="email"
@@ -419,7 +419,7 @@
 						/>
 					</div>
 					<div class="mb-4">
-						<label for="message" class="block text-gray-700 font-semibold">Bericht</label>
+						<label for="message" class="block font-semibold text-gray-700">Bericht</label>
 						<textarea
 							id="message"
 							name="message"
@@ -430,7 +430,7 @@
 					</div>
 					<button
 						type="submit"
-						class="py-3 px-5 bg-lime-600 hover:bg-lime-500 text-white rounded-lg send-btn"
+						class="px-5 py-3 text-white rounded-lg bg-lime-600 hover:bg-lime-500 send-btn"
 						>Verzenden</button
 					>
 				</form>
@@ -439,49 +439,57 @@
 	</div> -->
 	<div
 		id="contact"
-		class="mb-0 lg:mb-0 lg:h-screen last-screen flex content-center flex-col justify-center lg:text-lg text-l"
+		class="flex flex-col content-center justify-center gap-4 mb-0 lg:mb-0 lg:h-screen last-screen lg:text-lg text-l"
 	>
-		<h3 class="text-center font-bold text-lime-600 mt-16 lg:mt-0 contact-title1">Contacteer ons</h3>
-		<h2 class="text-center text-2xl my-4 mb-8 lg:mb-16 px-2 contact-title2">
-			Wenst u thuisverpleging aan te vragen, gebruik te maken van onze ondersteunende diensten of
-			heeft u een vraag? Aarzel dan niet om ons te contacteren!
-		</h2>
+		<div>
+			<h3 class="mt-16 font-bold text-center text-lime-600 lg:mt-0 contact-title1">
+				Contacteer ons
+			</h3>
+			<h2 class="px-2 my-4 mb-8 text-2xl text-center lg:mb-16 contact-title2">
+				Wenst u thuisverpleging aan te vragen, gebruik te maken van onze ondersteunende diensten of
+				heeft u een vraag? Aarzel dan niet om ons te contacteren!
+			</h2>
+		</div>
 		<div
 			class="flex flex-col items-center content-center mb-8 lg:mb-0 lg:justify-center lg:min-w-fit lg:px-4 contact-container"
 		>
 			<div
-				class="border-2 border-lime-500 rounded-full w-12 h-12 lg:w-20 lg:h-20 flex items-center justify-center mb-8"
+				class="flex items-center justify-center w-12 h-12 mb-8 border-2 rounded-full border-lime-500 lg:w-20 lg:h-20"
 			>
-				<img src="/phone-solid.svg" alt="" class="w-12 lg:w-12 m-4" />
+				<img src="/phone-solid.svg" alt="" class="w-12 m-4 lg:w-12" />
 			</div>
-			<div class="flex flex-row items-center mb-4 gap-4 justify-between">
+			<div class="flex flex-row items-center justify-between gap-4 mb-4">
 				<a href="tel:+32473952864" class="underline">0473 95 28 64</a>
 				<h3>An De Ro</h3>
 			</div>
 
-			<div class="flex flex-row items-center mb-4 gap-4 justify-between">
+			<div class="flex flex-row items-center justify-between gap-4 mb-4">
 				<a href="tel:+32496569360" class="underline">0496 56 93 60</a>
 				<h3>Vicky Van Biesen</h3>
 			</div>
 
-			<div class="flex flex-row items-center mb-4 gap-4 justify-between">
+			<div class="flex flex-row items-center justify-between gap-4 mb-4">
 				<a href="tel:+32473578230" class="underline">0473 57 82 30</a>
 				<h3>Kelly van Cutsem</h3>
 			</div>
 
-			<div class="flex flex-row items-center mb-4 gap-4 justify-between">
+			<div class="flex flex-row items-center justify-between gap-4 mb-4">
 				<a href="tel:+32472450324" class="underline">0472 45 03 24</a>
 				<h3>Yilke Read</h3>
 			</div>
 		</div>
 	</div>
 </main>
-<footer class="bg-gray-200 py-4 mt-8 lg:mt-8">
+<footer class="py-4 mt-8 bg-gray-200 lg:mt-8">
 	<div class="lg:px-24">
-		<div class="flex flex-col justify-center items-center">
-			<a href="https://www.instagram.com/thuisverpleging_nova/" target="_blank" class="mb-2">
-				<img src="/instagram.svg" alt="instagram icon" class="w-10" />
-			</a>
+		<div class="flex flex-col items-center justify-center">
+			<div class="flex flex-row gap-4">
+				<a href="https://www.instagram.com/thuisverpleging_nova/" target="_blank" class="mb-2">
+					<img src="/instagram.svg" alt="instagram icon" class="w-10" />
+				</a><a href="https://www.facebook.com/ThuisverplegingNova" target="_blank" class="mb-2">
+					<img src="/facebook.svg" alt="facebook icon" class="w-10" />
+				</a>
+			</div>
 			<p class="text-gray-600">&copy; 2023 Nova. Alle rechten voorbehouden.</p>
 		</div>
 	</div>
@@ -501,9 +509,6 @@
 	}
 
 	.contact-btn {
-		transition: background-color 0.3s ease;
-	}
-	.send-btn {
 		transition: background-color 0.3s ease;
 	}
 </style>
